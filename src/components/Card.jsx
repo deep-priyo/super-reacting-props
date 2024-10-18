bebe80f59c47a374b684be3f08f08f938b74c986
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-10-19 00:44:29 +0530

    added basic alert to button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 
-const Card = ({ name, profession, img }) => {
-
+const Card = ({ values }) => {
+    const { name, profession, img } = values
     return (
         <div className="w-52 bg-white rounded-md overflow-hidden">
             <div className="w-full h-40 bg-sky-200">
@@ -9,12 +9,12 @@ const Card = ({ name, profession, img }) => {
             <div className="w-full p-3 ">
                 <h3 className="text-xl font-semibold">{name}</h3>
                 <h5 className="text-xs">{profession}</h5>
-                <button className="px-3 py-1 text-xs text-white bg-blue-500 rounded-md mt-3">Add Friend</button>
+                <button onClick={() => (alert('Adding as a freind'))} className="px-3 py-1 text-xs text-white bg-blue-500 rounded-md mt-3">Add Friend</button>
             </div>
 
 
         </div>
     );
 };
-src/
+
 export default Card;
