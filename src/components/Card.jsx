cd5e104185190f2a72d2fb716c136dc4290751c3
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-10-19 01:16:52 +0530

    added resposive friend/add friends with dynamic color

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,21 @@
+const Card = ({ values, func, index }) => {
+    const { name, profession, img, freind } = values; // Note: using `freind` instead of `friends`
 
-const Card = ({ values }) => {
-    const { name, profession, img } = values
     return (
         <div className="w-52 bg-white rounded-md overflow-hidden">
             <div className="w-full h-40 bg-sky-200">
-                <img className='w-full h-full object-center' src={img} alt="Image of person" />
+                <img className="w-full h-full object-center" src={img} alt="Image of person" />
             </div>
-            <div className="w-full p-3 ">
+            <div className="w-full p-3">
                 <h3 className="text-xl font-semibold">{name}</h3>
                 <h5 className="text-xs">{profession}</h5>
-                <button onClick={() => (alert('Adding as a freind'))} className="px-3 py-1 text-xs text-white bg-blue-500 rounded-md mt-3">Add Friend</button>
+                <button
+                    onClick={() => func(index)}
+                    className={`px-3 py-1 text-xs text-white  ${freind ? 'bg-blue-500' : 'bg-green-500'} rounded-md mt-3`}
+                >
+                    {freind ? "Friends" : "Add Friends"}
+                </button>
             </div>
-
-
         </div>
     );
 };
